f20ad82a59700cfb26837e79e0b5a7c123a6a6a0
Xelden <anpiz@protonmail.com>
2024-02-16 13:35:02 -0500

    Temporary solution for failed langchain build

--- flake.nix
+++ flake.nix
@@ -2,31 +2,32 @@
 {
   description = "PDF trained Chat-Bot with LLM";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-langchain1-0.url = "github:nixos/nixpkgs/4d3d04cfd372a0511f12b885c9822e406b00fe8e";
+  };
 
-  outputs = { self, nixpkgs }:
-    let
-      pkgs = import nixpkgs {
-        system = "x86_64-linux";
-      };
-      pythonPackages = pkgs.python311Packages;
-      pyPkgs = with pythonPackages; [
-        huggingface-hub
-        streamlit
-        python-dotenv
-        pypdf2
-        faiss
-        #langchain
-      ];
-    in
-    {
-      devShells.x86_64-linux = {
-        default = pkgs.mkShell {
-          buildInputs = [ 
-            pyPkgs
-
-          ];
-        };
+  outputs = { self, nixpkgs, ... } @inputs:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+    pythonPackages = pkgs.python311Packages;
+    pyPkgs = with pythonPackages; [
+      huggingface-hub
+      streamlit
+      python-dotenv
+      pypdf2
+      faiss
+      # langchain
+    ] ++ [ inputs.nixpkgs-langchain1-0.legacyPackages.${system}.python311Packages.langchain ]; # Old langchain version
+  in
+  {
+    devShells.${system} = {
+      default = pkgs.mkShell {
+        buildInputs = [ 
+          pyPkgs
+        ];
       };
     };
+  };
 }
